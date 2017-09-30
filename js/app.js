97a9e3fb2004ee3331db0d4ace331b33171dd04d
michaeluduhiri <30484067+michaeluduhiri@users.noreply.github.com>
2017-09-30 15:40:54 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -101,7 +101,7 @@ function endGame(cardMoves, score) {
 		allowEscapeKey: false,
 		allowOutsideClick: false,
 		title: 'Congratulations! You Won!',
-		text: 'With ' + moves + ' Moves and ' + score + ' Stars.\n Woooooo!',
+		text: 'With ' + cardMoves + ' Moves and ' + score + ' Stars.\n Woooooo!',
 		type: 'success',
 		confirmButtonColor: '#02ccba',
 		confirmButtonText: 'Play again!'
